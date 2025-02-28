bb19a8c06e3cfe97a3e6b449580e49a33fe994d8
Alvarado007 <samuel.alvarado@correounivalle.edu.co>
2025-02-28 07:48:04 -0500

    inicializacion del proyecto con spring boot

---
+++ src/main/java/com/alvaradito/spring/primerproyecto/springboot_primerproyecto/SpringbootPrimerproyectoApplication.java
@@ -0,0 +1,13 @@
+package com.alvaradito.spring.primerproyecto.springboot_primerproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPrimerproyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPrimerproyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvaradito/spring/primerproyecto/springboot_primerproyecto/SpringbootPrimerproyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvaradito.spring.primerproyecto.springboot_primerproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPrimerproyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
