c82382399d9582a4064db4afd3c10d5ac18a141e
Alvarado007 <samuel.alvarado@correounivalle.edu.co>
2025-03-07 08:39:07 -0500

    Se trabaj con la anotacion Value y se cambia el rpoyecto iyectando datos desde aplication.properties

--- src/main/java/com/alvaradito/spring/primerproyecto/springboot_primerproyecto/controllers/PathVariableController.java
+++ src/main/java/com/alvaradito/spring/primerproyecto/springboot_primerproyecto/controllers/PathVariableController.java
@@ -1,5 +1,9 @@
 package com.alvaradito.spring.primerproyecto.springboot_primerproyecto.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +13,26 @@ import com.alvaradito.spring.primerproyecto.springboot_primerproyecto.models.emp
 import com.alvaradito.spring.primerproyecto.springboot_primerproyecto.models.dto.ParametroDTO;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
 @RestController
 @RequestMapping("/api/variable")
 public class PathVariableController {
+    @Value("${config.usuario}")
+    private String usuario;
+
+    @Value("${config.mensaje}")
+    private String mensaje;
+
+    @Value("${config.codigo}")
+    private int codigo;
+
+    @Value("${config.valores}")
+    private String[] valores;
+
 
     @GetMapping("/pagina1/{mensaje}")//mensaje
     public ParametroDTO pagina1(@PathVariable String mensaje) {
@@ -26,6 +44,14 @@ public class PathVariableController {
     public empleados CrearEmpleado(@RequestBody empleados empleado3){
         return empleado3;
     }
-    
-    
+
+    @GetMapping("/valores")//valores
+    public Map<String, Object> values(){
+    Map<String, Object> json= new HashMap<>();
+    json.put("usuario", usuario);
+    json.put("mensaje", mensaje);
+    json.put("codigo", codigo);
+    json.put("valores", valores);
+    return json;
+    }
 }
